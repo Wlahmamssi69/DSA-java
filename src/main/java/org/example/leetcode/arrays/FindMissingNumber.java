7255733fe27dbecc51155a18368085de8c90a32b
Wlahmamssi69 <wafa.lahmamssi@outlook.com>
2024-11-14 23:53:17 +0100

    Add missing number coding problem

---
+++ src/main/java/org/example/leetcode/arrays/FindMissingNumber.java
@@ -0,0 +1,21 @@
+package org.example.leetcode.arrays;
+
+public class FindMissingNumber {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,6};
+        System.out.println("the missing number : "+findMissingNumber(array,6));
+
+    }
+    public static int findMissingNumber(int[] arr) {
+        int size = arr.length;
+        int sum = 0;
+        for(int i=0;i<size;i++){
+            sum+=arr[i];
+        }
+        //calculate the first natural number to size
+        int sumOfNaturalNumbers = ((size+1)*(size+2))/2;
+        return sumOfNaturalNumbers - sum;
+    }
+
+
+}
